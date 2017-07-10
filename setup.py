398e0fc32e6bb5ffe969fcaf720761c1131cdb00
Nicholas Cole <nicholas.cole@history.ox.ac.uk>
2017-07-10 12:11:23 +0100

    Updating ready for a new release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name="npyscreen",
-	version="4.11",
+	version="4.12",
 	description="Writing user interfaces without all that ugly mucking about in hyperspace",
 	author="Nicholas Cole",
 	author_email="n@npcole.com",
@@ -38,6 +38,7 @@ There is a mailing list available at https://groups.google.com/forum/?fromgroups
 
 
 *Latest Changes*:
+Version 4.12: Includes some contributed bug fixes.
 
 Version 4.11: Sliders can return ints as well as floats.
 
